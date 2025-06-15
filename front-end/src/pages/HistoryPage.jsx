1cc0ac4b3890eb60b5d973f0ecc18210cd3abf27
Michal Heller <michalhe07@gmail.com>
2025-06-15 13:31:09 +0300

    added history page

--- front-end/src/pages/HistoryPage.jsx
+++ front-end/src/pages/HistoryPage.jsx
@@ -1,6 +1,38 @@
+import { useState, useContext, useEffect } from "react";
+
+import UserContext from "../store/user-context";
+import useHttp from "../hooks/use-http";
+import RequestList from "../components/Requests/RequestList";
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+
 const HistoryPage = () => {
+   const { sendRequest, isLoading } = useHttp();
+
+   const { token } = useContext(UserContext);
+
+   const [requests, setRequests] = useState([]);
+
+   useEffect(() => {
+      sendRequest({
+         url: `http://localhost:8000/requests?state=approved,rejected`,
+         headers: {
+            'Authorization': token
+         }
+      }, (data) => {
+         setRequests(data);
+      });
+   }, [sendRequest, token]);
+
+   if (isLoading) {
+      return <LoadingSpinner />;
+   }
+
+   if (requests.length === 0) {
+      return <p>כאן תופיע הסטוריית הבקשות.</p>;
+   }
+
    return (
-      <div></div>
+      <RequestList requests={requests} showDate showUser />
    );
 }
 
