33e150a897b52ce17ed5d0fe48d4d016e8f3620d
Michal Heller <michalhe07@gmail.com>
2025-06-16 10:14:35 +0300

    added logout timer to be refined later

--- front-end/src/store/user-context.js
+++ front-end/src/store/user-context.js
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 
+let logoutTimer;
+
 const UserContext = createContext({
    user: null,
    token: '',
@@ -7,32 +9,66 @@ const UserContext = createContext({
    logout: () => {}
 });
 
+const calculateRemainingTime = (expirationTime) => {
+   const currentTime = new Date().getTime();
+   const adjExpirationTime = new Date(expirationTime).getTime();
+
+   const remainingDuration = adjExpirationTime - currentTime;
+
+   return remainingDuration;
+}
+
 export const UserCtxProvider = (props) => {
    const [user, setUser] = useState(null);
    const [token, setToken] = useState('');
 
    useEffect(() => {
+      const storedExpirationDate = localStorage.getItem('expirationTime');
+      const remainingTime = calculateRemainingTime(storedExpirationDate);
+
+      if (remainingTime <= 60000) {
+         localStorage.removeItem('token');
+         localStorage.removeItem('user');
+         localStorage.removeItem('expirationTime');
+         return;
+      }
+      
+      logoutTimer = setTimeout(logoutHandler, remainingTime);
+      
       const storedUser = JSON.parse(localStorage.getItem('user'));
       setUser(storedUser);
-
+      
       const storedToken = localStorage.getItem('token');
       setToken(storedToken ? storedToken : '');
    }, []);
-
+   
    const loginHandler = (user, token) => {
       setUser(user);
       setToken(token);
-
+      
+      // enum to come
+      const expiresIn = new Date(new Date().getTime() + 10800000).toISOString();
+      
+      const remainingTime = calculateRemainingTime(expiresIn);
+      
+      logoutTimer = setTimeout(logoutHandler, remainingTime);
+      
       localStorage.setItem('user', JSON.stringify(user));
       localStorage.setItem('token', token);
+      localStorage.setItem('expirationTime', expiresIn);
    }
-
+   
    const logoutHandler = () => {
       setUser(null);
       setToken('');
-
+      
       localStorage.removeItem('user');
       localStorage.removeItem('token');
+      localStorage.removeItem('expirationTime');
+
+      if (logoutTimer) {
+         clearTimeout(logoutTimer);
+      }
    }
 
    const contextValue = {
